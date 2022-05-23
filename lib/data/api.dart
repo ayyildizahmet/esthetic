85b5515468066e1a514a0b225553d4d75143b7ea
FlutLab.io <>
2022-05-23 10:46:12 +0000

    Autosave

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -6,34 +6,28 @@ import 'model/user_login_request_model.dart';
 import 'model/user_login_response_model.dart';
 
 class ApiService {
-  final String loginURL =
-      "http://ayyildiz.xyz:8090/api/auth/login"; //"https://reqres.in/api/login";
+  final String loginURL = "http://ayyildiz.xyz:8090/api/auth/login"; //"https://reqres.in/api/login";
 
-  Future<UserLoginResponseModel> login(
-      UserLoginRequestModel userLoginRequestModel) async {
+  Future<UserLoginResponseModel> login(UserLoginRequestModel userLoginRequestModel) async {
     print(userLoginRequestModel.toJson());
     Response res = await post(Uri.parse(loginURL),
-        headers: {"Content-Type": "application/json"},
-        body: jsonEncode(userLoginRequestModel.toJson()));
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: jsonEncode(userLoginRequestModel.toJson()))
+        .timeout(const Duration(seconds: 10));
     if (res.statusCode == 200 || res.statusCode == 400) {
-      UserLoginResponseModel posts =
-          UserLoginResponseModel.fromJson(jsonDecode(res.body));
+      UserLoginResponseModel posts = UserLoginResponseModel.fromJson(jsonDecode(res.body));
       return posts;
     } else {
       throw "Unable to retrieve posts.";
     }
   }
 
-  Future<UserSignupResponseModel> signin(
-      UserSignupRequestModel userSigninRequestModel) async {
-    print(userSigninRequestModel.toJson());
-    Response res =
-        await post(Uri.parse(loginURL), body: userSigninRequestModel.toJson());
-    print("status = " + res.statusCode.toString());
+  Future<UserSignupResponseModel> signup(UserSignupRequestModel userSignupRequestModel) async {
+    Response res = await post(Uri.parse(loginURL), body: userSignupRequestModel.toJson()).timeout(const Duration(seconds: 10));
     if (res.statusCode == 200 || res.statusCode == 400) {
-      UserSignupResponseModel posts =
-          UserSignupResponseModel.fromJson(jsonDecode(res.body));
-      print(posts.isSuccess);
+      UserSignupResponseModel posts = UserSignupResponseModel.fromJson(jsonDecode(res.body));
       return posts;
     } else {
       throw "Unable to retrieve posts.";
