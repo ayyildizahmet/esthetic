139d7fc2d174ad7da8a43cc652102bda10dcf43c
FlutLab.io <>
2022-05-25 11:08:45 +0000

    Autosave

--- lib/data/model/user_signup_response_model.dart
+++ lib/data/model/user_signup_response_model.dart
@@ -3,15 +3,23 @@ import 'dart:convert';
 List<UserSignupResponseModel> userFromJson(String str) => List<UserSignupResponseModel>.from(json.decode(str).map((x) => UserSignupResponseModel.fromJson(x)));
 String userToJson(List<UserSignupResponseModel> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class UserSignupResponseModel {
-  UserSignupResponseModel({required this.isSuccess});
+class UserSignupResponseModel extends BaseResponseModel {
+  UserSignupResponseModel({success, message, required this.isSuccess})
+      : super(
+          success: success,
+          message: message,
+        );
 
   bool isSuccess;
 
   factory UserSignupResponseModel.fromJson(Map<String, dynamic> json) => UserSignupResponseModel(
+        success: json["success"],
+        message: json["message"],
         isSuccess: json["isSuccess"] ?? "",
       );
   Map<String, dynamic> toJson() => {
+        "success": success,
+        "message": message,
         "isSuccess": isSuccess
       };
 }
