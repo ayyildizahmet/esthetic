1f44bb56e2d098709d163207205ec57a8c08e55b
FlutLab.io <>
2022-05-30 19:24:48 +0000

    Autosave

--- lib/screens/activity.dart
+++ lib/screens/activity.dart
@@ -1,3 +1,4 @@
+import 'package:esthetic/widgets/bubble_stories.dart';
 import 'package:flutter/material.dart';
 
 class ActivityScreen extends StatefulWidget {
@@ -6,8 +7,31 @@ class ActivityScreen extends StatefulWidget {
 }
 
 class _ActivityScreen extends State<ActivityScreen> {
+  final List companies = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e"
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home")));
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+              height: 130,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: companies.length,
+                itemBuilder: (context, index) {
+                  return BubbleStories();
+                },
+              ))
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/bubble_stories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BubbleStories extends StatefulWidget {
+  @override
+  _BubbleStories createState() => _BubbleStories();
+}
+
+class _BubbleStories extends State<BubbleStories> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.grey[400]),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text('name')
+        ],
+      ),
+    );
+  }
+}
