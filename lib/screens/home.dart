f1ea2727f0fc214073638a0acc0f845cf1ab5778
FlutLab.io <>
2022-05-30 12:15:49 +0000

    Autosave

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,6 @@
 import 'package:esthetic/data/model/home_bottom_navigation_menu_model.dart';
-import 'package:esthetic/utilities/color.dart';
-import 'package:esthetic/widgets/custom_paint.dart';
+import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,89 +11,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextStyle bntText = const TextStyle(
-    color: black,
-    fontWeight: FontWeight.w500,
-  );
+  int _selectedIndex = 0;
 
-  int selectBtn = 0;
+  void navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: navigationBar(),
-          )
-        ],
+      body: const Center(
+        child: const Text('Body Here'),
       ),
-    );
-  }
-
-  AnimatedContainer navigationBar() {
-    return AnimatedContainer(
-      height: 70.0,
-      duration: const Duration(milliseconds: 400),
-      decoration: BoxDecoration(
-        color: white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(selectBtn == 0 ? 0.0 : 20.0),
-          topRight: Radius.circular(selectBtn == navBtn.length - 1 ? 0.0 : 20.0),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          for (int i = 0; i < navBtn.length; i++)
-            GestureDetector(
-              onTap: () => setState(() => selectBtn = i),
-              child: iconBtn(i),
-            ),
-        ],
-      ),
-    );
-  }
-
-  SizedBox iconBtn(int i) {
-    bool isActive = selectBtn == i ? true : false;
-    var height = isActive ? 60.0 : 0.0;
-    var width = isActive ? 50.0 : 0.0;
-    return SizedBox(
-      width: 75.0,
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: AnimatedContainer(
-              height: height,
-              width: width,
-              duration: const Duration(milliseconds: 600),
-              child: isActive
-                  ? CustomPaint(
-                      painter: ButtonNotch(),
-                    )
-                  : const SizedBox(),
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              navBtn[i].imagePath,
-              color: isActive ? selectColor : black,
-              scale: 2,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              navBtn[i].name,
-              style: isActive ? bntText.copyWith(color: selectColor) : bntText,
-            ),
-          )
-        ],
-      ),
+      bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed, currentIndex: _selectedIndex, onTap: navigateBottomNavBar, items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Shop"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Forum"),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Profile"),
+      ]),
     );
   }
 }
