0d62d804887354ad8bcfe5f4ee5f1f7f065ed5b2
FlutLab.io <>
2022-06-08 07:50:03 +0000

    Autosave

--- lib/screens/add_operation.dart
+++ lib/screens/add_operation.dart
@@ -15,40 +15,58 @@ class AddOperationScreen extends StatefulWidget {
 class _AddOperationScreen extends State<AddOperationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'Clinic',
-          style: kLabelStyle,
+    return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Colors.grey[100],
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Add Operation Record', style: TextStyle(color: Colors.black)),
+            Row(
+              children: const [
+                Icon(Icons.add, color: Colors.black),
+                Padding(padding: EdgeInsets.all(24), child: Icon(Icons.favorite, color: Colors.black)),
+                Icon(Icons.share, color: Colors.black)
+              ],
+            )
+          ],
         ),
-        const SizedBox(height: 10.0),
-        Container(
-          alignment: Alignment.centerLeft,
-          decoration: kBoxDecorationStyle,
-          height: 60.0,
-          child: TextFormField(
-            //validator: emailValidator,
-            //onChanged: (input) => _email = input, //input,
-            keyboardType: TextInputType.emailAddress,
-            style: const TextStyle(
-              color: Colors.white,
-              fontFamily: 'OpenSans',
+      ),,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Clinic',
+              style: kLabelStyle,
             ),
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              contentPadding: const EdgeInsets.only(top: 14.0),
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.white,
+            const SizedBox(height: 10.0),
+            Container(
+              alignment: Alignment.centerLeft,
+              decoration: kBoxDecorationStyle,
+              height: 60.0,
+              child: TextFormField(
+                //validator: emailValidator,
+                //onChanged: (input) => _email = input, //input,
+                keyboardType: TextInputType.emailAddress,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'OpenSans',
+                ),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.only(top: 14.0),
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  errorStyle: const TextStyle(color: Colors.white, fontFamily: 'OpenSans', decorationColor: Colors.white),
+                  hintText: 'Enter your Email',
+                  hintStyle: kHintTextStyle,
+                ),
               ),
-              errorStyle: const TextStyle(color: Colors.white, fontFamily: 'OpenSans', decorationColor: Colors.white),
-              hintText: 'Enter your Email',
-              hintStyle: kHintTextStyle,
             ),
-          ),
-        ),
-      ],
-    );
+          ],
+        ));
   }
 }
