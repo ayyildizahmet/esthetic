93722ca26f7c331a716feba119d06e4e75ea0e9b
FlutLab.io <>
2022-06-10 06:54:41 +0000

    Autosave

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xFF6CA8F1);
+const Color kPrimaryColor = Colors.green;
 const Color kTextColor = Color(0xFF3C4046);
 const Color kBackgroundColor = Color(0xFFF9F8FD);
 
